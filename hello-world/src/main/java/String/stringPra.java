0a06f802fd9171872c99622dcfb3894083bc7286
hezongkui <zongkuihe@Gmail.com>
2019-09-12 18:09:31 +0800

    test on serveice

--- hello-world/src/main/java/String/stringPra.java
+++ hello-world/src/main/java/String/stringPra.java
@@ -1,16 +1,12 @@
 package String;
 
 public class stringPra {
+
     public static void main(String[] args) {
-        String str="a";
-        String dasb = str.concat("das");
-        char[] chars = dasb.toCharArray();
-        int i = 2;
-        System.out.println(chars.length);
-        for (;i<chars.length;i++) {
-           System.out.println(chars[i]);
+
+        System.out.println("nihao ");
             
         }
 
-    }
+
 }
