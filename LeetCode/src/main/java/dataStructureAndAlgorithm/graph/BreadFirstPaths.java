5747190dbf7a9207bbf09e445a066b48a6ad952d
hezongkui <zongkuihe@Gmail.com>
2019-12-31 17:33:46 +0800

    图的代码练习

---
+++ LeetCode/src/main/java/dataStructureAndAlgorithm/graph/BreadFirstPaths.java
@@ -0,0 +1,45 @@
+package dataStructureAndAlgorithm.graph;
+
+import algs4.Queue;
+
+/**
+ * @author hzk
+ */
+public class BreadFirstPaths {
+    //到达该顶点的最短路径已知吗
+    private boolean[] marked;
+    //到达该顶点的已知路径上的最后一个顶点
+    private int[] edgeTo;
+    //起点
+    private int s;
+
+    public BreadFirstPaths(Graph G, int s) {
+        marked = new boolean[G.getV()];
+        edgeTo = new int[G.getV()];
+        this.s = s;
+        bfs(G, s);
+    }
+    public void bfs(Graph G, int s) {
+        Queue<Integer> queue = new Queue<Integer>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[v]) {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        return null;
+    }
+}
